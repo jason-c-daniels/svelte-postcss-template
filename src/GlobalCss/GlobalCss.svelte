520dc9a912043da7e4114daffa116aeead60f13c
Jason Daniels <jason.c.daniels@gmail.com>
2020-08-09 10:30:43 -0500

    created and used custom properties.

--- src/GlobalCss/GlobalCss.svelte
+++ src/GlobalCss/GlobalCss.svelte
@@ -1,4 +1,18 @@
-<style global="true">
-    @import "normalize.css";
+<!-- Put imports of global CSS in here.
+     By default normalize.css and global.css are imported.
+     Other valid items to consider are font-face declarations.
+     normalize.css is a type of reset.css. Tweak it as appropriate to your project.
+     If you're looking for an opinionated set of cross browser consistent settings,
+     use sanitize.css instead of normalize.
+
+     global.css defines global custom variables. If you are tempted to put style overrides
+     to normalize.css in it, don't do it. it violates the DRY principle.
+
+    TL;DR: remember to only use one of 'reset.css', 'normalize.css', 'sanitize.css'.
+    put global css variables in global.css.
+-->
+
+<style global>
     @import "global.css";
+    @import "normalize.css";
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
+		appSettings:{ applicationName: "Svelte PostCSS demo app"},
 		name: 'world'
 	}
 });
